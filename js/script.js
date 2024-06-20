599bce98b5cb2485f69f626dd1d360d9d99e4f06
j4ckrisz <frenshystyle255@protonmail.com>
2024-06-19 20:48:21 -0500

    Updating password generator js and style

--- js/script.js
+++ js/script.js
@@ -1,52 +1,49 @@
-function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSimbols ){
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
 
-    const lowercaseChars = "abcdefghijklmnopqrstuvwsyz";
-    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWSYZ";
-    const numberChars = "013456789";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
     const symbolChars = "!@#$%^&*()_+-=";
 
     let allowedChars = '';
     let password = '';
 
-    allowedChars += includeLowercase ? lowercaseChars : "";
-    allowedChars += includeUppercase ? uppercaseChars : "";
-    allowedChars += includeNumbers ? numberChars : "";
-    allowedChars += includeSimbols ? symbolChars : "";
+    if (includeLowercase) allowedChars += lowercaseChars;
+    if (includeUppercase) allowedChars += uppercaseChars;
+    if (includeNumbers) allowedChars += numberChars;
+    if (includeSymbols) allowedChars += symbolChars;
 
-    if (length <= 0){
-        return `(password lenght must be at least 1)`
+    if (length <= 0) {
+        return `(password length must be at least 1)`;
     }
 
-    if (allowedChars.length === 0){
-
-        return `(At least 1 set of characters needs to be selected)`
-
+    if (allowedChars.length === 0) {
+        return `(At least 1 set of characters needs to be selected)`;
     }
 
-    for(let i =0; i< length; i++){
+    function getRandomIndex(max) {
+        const array = new Uint32Array(1);
+        crypto.getRandomValues(array);
+        return array[0] % max;
+    }
 
-        const randomIndex = Math.floor( Math.random() * allowedChars.length);
-        password += allowedChars[randomIndex]
-
+    for (let i = 0; i < length; i++) {
+        const randomIndex = getRandomIndex(allowedChars.length);
+        password += allowedChars[randomIndex];
     }
 
     return password;
+};
 
-}
+document.getElementById('generateButton').onclick = function() {
+  
+    const length = parseInt(document.getElementById('passwordLength').value, 10);
+    const includeLowercase = document.getElementById('lowercase').checked;
+    const includeUppercase = document.getElementById('uppercase').checked;
+    const includeNumbers = document.getElementById('numbers').checked;
+    const includeSymbols = document.getElementById('symbols').checked;
 
+    const password = generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
 
-const passwordLenght = 12;
-const includeLowercase = true;
-const includeUppercase = true;
-const includeNumbers = true;
-const includeSimbols = true;
-
-const password = generatePassword(passwordLenght, 
-                                includeLowercase, 
-                                includeUppercase, 
-                                includeNumbers, 
-                                includeSimbols);
-
-console.log(`Generated password : ${password}`)
-
-
+    document.getElementById('passwordOutput').textContent = password;
+};
